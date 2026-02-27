ce3b67d94f086ec3258cc3bb0974c7547d32e9f4
Sophia da Costa Fernandes <sophiaferch@gmail.com>
2026-02-27 16:35:35 -0300

    update exception

--- src/com/sophia/aulapm/lab02/TestandoException.java
+++ src/com/sophia/aulapm/lab02/TestandoException.java
@@ -7,12 +7,12 @@ public class TestandoException {
 	public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         double nota;
-        double frequencia; // Inicializamos com valor inválido para entrar no loop
+        double frequencia; // valor invalido para entrar no loop
 
         System.out.println("Digite a nota do aluno: ");
         nota = scan.nextDouble();
 
-        // Loop de repetição para entrada válida
+        // Loop para entrada valida
         while (true) {
             try {
                 System.out.println("Digite a frequencia do aluno (0.0 a 1.0): ");
@@ -25,13 +25,10 @@ public class TestandoException {
                 break; 
                 
             } catch (FrequenciaException e) {
-                // Aqui tratamos o SEU erro específico
-                //System.out.println(e.getMessage());
                 System.out.println("Tente novamente...");
             } catch (Exception e) {
-                // Rede de segurança para entradas que não são números
+                
                 System.out.println("Entrada inválida! Digite apenas números.");
-                scan.next(); // Limpa o buffer do scanner
             }
         }
 
@@ -43,14 +40,12 @@ public class TestandoException {
         if(nota >= 7.0 && frequencia >= 0.75) {
             System.out.println("Aprovado");
         } else if(nota >= 4.0 && frequencia >= 0.75) { 
-            // Dica: Simplifiquei sua lógica de recuperação
             System.out.println("Em recuperação");
         } else {
             System.out.println("Reprovado");
         }   
     }
-    
-    // Agora o método não retorna boolean, ele é um "Guarda"
+
     static void validarFrequencia(double frequencia){
         if(frequencia > 1 || frequencia < 0) {
             throw new FrequenciaException(frequencia);

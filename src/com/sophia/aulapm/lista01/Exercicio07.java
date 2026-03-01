d8c9fec80310d5a0e9c33952fcb6367564bfec8c
Sophia da Costa Fernandes <sophiaferch@gmail.com>
2026-03-01 15:31:16 -0300

    update ex07

--- src/com/sophia/aulapm/lista01/Exercicio07.java
+++ src/com/sophia/aulapm/lista01/Exercicio07.java
@@ -10,16 +10,21 @@ public class Exercicio07 {
 
 		double[] mesTemp = new double[12];
 
-		System.out.println("Digite as temperaturas: ");
-		for (int i = 1; i < mesTemp.length; i++) {
-			System.out.println("Digite a temperatura do mes " + i);
+		for (int i = 0; i < mesTemp.length; i++) {
+			System.out.println("Digite a temperatura do mês " + (i + 1) + ": ");
 			mesTemp[i] = sc.nextDouble();
 		}
+		
+		double maiorTemp = encontraMaiorTemp(mesTemp);
+		mostraMaior(maiorTemp); 
+		
+		double menorTemp = encontraMenorTemp(mesTemp);
+		mostraMenor(menorTemp);
 	}
 
 	public static double encontraMaiorTemp(double[] a) {
-		double maiorTemp = a[1];
-		for (int i = 1; i < a.length; i++) {
+		double maiorTemp = a[0];
+		for (int i = 0; i < a.length; i++) {
 			if (a[i] > maiorTemp) {
 				maiorTemp = a[i];
 				return i;
@@ -29,8 +34,8 @@ public class Exercicio07 {
 	}
 
 	public static double encontraMenorTemp(double[] a) {
-		double menorTemp = a[1];
-		for (int i = 1; i < a.length; i++) {
+		double menorTemp = a[0];
+		for (int i = 0; i < a.length; i++) {
 			if (a[i] < menorTemp) {
 				menorTemp = a[i];
 				return i;
@@ -39,5 +44,86 @@ public class Exercicio07 {
 		return menorTemp;
 	}
 	
-	//metodo para o mes por extenso?
+	public static void mostraMaior(double maior) {
+		if(maior == 0) {
+			System.out.println("O mês com a maior temperatura é janeiro ");
+		} 
+		if(maior ==1) {
+			System.out.println("O mês com a maior temperatura é Fevereiro ");
+		}
+		if(maior ==2) {
+			System.out.println("O mês com a maior temperatura é Março ");
+		}
+		if(maior ==3) {
+			System.out.println("O mês com a maior temperatura é Abril ");
+		}
+		if(maior ==4) {
+			System.out.println("O mês com a maior temperatura é Maio ");
+		}
+		if(maior ==5) {
+			System.out.println("O mês com a maior temperatura é Junho ");
+		}
+		if(maior ==6) {
+			System.out.println("O mês com a maior temperatura é Julho ");
+		}
+		if(maior ==7) {
+			System.out.println("O mês com a maior temperatura é Agosto ");
+		}
+		if(maior ==8) {
+			System.out.println("O mês com a maior temperatura é Setembro ");
+		}
+		if(maior ==9) {
+			System.out.println("O mês com a maior temperatura é Outubro ");
+		}
+		if(maior ==10) {
+			System.out.println("O mês com a maior temperatura é Novembro ");
+		}
+		if(maior ==11) {
+			System.out.println("O mês com a maior temperatura é Dezembro ");
+		}
+		
+	}
+	
+	public static void mostraMenor(double menor) {
+		
+		if(menor == 0) {
+			System.out.println("O mês com a menor temperatura é janeiro ");
+		} 
+		if(menor ==1) {
+			System.out.println("O mês com a menor temperatura é Fevereiro ");
+		}
+		if(menor ==2) {
+			System.out.println("O mês com a menor temperatura é Março ");
+		}
+		if(menor ==3) {
+			System.out.println("O mês com a menor temperatura é Abril ");
+		}
+		if(menor ==4) {
+			System.out.println("O mês com a menor temperatura é Maio ");
+		}
+		if(menor ==5) {
+			System.out.println("O mês com a menor temperatura é Junho ");
+		}
+		if(menor ==6) {
+			System.out.println("O mês com a menor temperatura é Julho ");
+		}
+		if(menor ==7) {
+			System.out.println("O mês com a menor temperatura é Agosto ");
+		}
+		if(menor ==8) {
+			System.out.println("O mês com a menor temperatura é Setembro ");
+		}
+		if(menor ==9) {
+			System.out.println("O mês com a menor temperatura é Outubro ");
+		}
+		if(menor ==10) {
+			System.out.println("O mês com a menor temperatura é Novembro ");
+		}
+		if(menor ==11) {
+			System.out.println("O mês com a menor temperatura é Dezembro ");
+		}
+		
+		
+	}
+
 }

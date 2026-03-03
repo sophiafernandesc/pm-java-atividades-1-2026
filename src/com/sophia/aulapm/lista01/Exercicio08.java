d00d63f3f7bc71710c109540fc27563ae64caa3e
Sophia da Costa Fernandes <sophiaferch@gmail.com>
2026-03-02 22:12:13 -0300

    update ex08

--- src/com/sophia/aulapm/lista01/Exercicio08.java
+++ src/com/sophia/aulapm/lista01/Exercicio08.java
@@ -3,61 +3,92 @@ package com.sophia.aulapm.lista01;
 import java.util.Scanner;
 
 public class Exercicio08 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		double[] valorObjetos = new double[2];
 		int[] qtdObjetos = new int[2];
-		
-		for( int i = 0; i < valorObjetos.length; i++) {
-			for(int j = 0; j < qtdObjetos.length; j++) {
-				System.out.println("Digite o valor do objeto " + (i+1) + " :");
-				valorObjetos[i] = sc.nextDouble();
-				System.out.println("Digite a quantidade vendida desse objeto: ");
-				qtdObjetos[i] = sc.nextInt();
-			}
+
+		for (int i = 0; i < valorObjetos.length; i++) {
+			System.out.println("Digite o valor do objeto " + (i + 1) + " :");
+			valorObjetos[i] = sc.nextDouble();
 		}
-		
+		for (int j = 0; j < qtdObjetos.length; j++) {
+			System.out.println("Digite a quantidade vendida do objeto " + (j+1));
+			qtdObjetos[j] = sc.nextInt();
+		}
+
 		int indiceMaisVendido = retornaIndiceMaisVendido(qtdObjetos);
-		mostraMaisVendido(indiceMaisVendido, valorObjetos);
-		System.out.println("O índice do objeto mais vendido no vetor é: " + indiceMaisVendido);
-		
+		mostraValorMaisVendido(indiceMaisVendido, valorObjetos);
+		mostraIndiceMaisVendido(indiceMaisVendido);
+
 		double totalVendas = calculaValorVendas(valorObjetos);
-		System.out.println("O valor total das vendas é: R$" + totalVendas);
+		mostraValorVendas(totalVendas);
+		
+		double comissao = calculaComissao(totalVendas);
+		mostraComissao(comissao);
+		
 	}
 	
+	//TO-DO Um relatório na tela contendo: quantidade vendida, valor unit´ario e valor total de cada objeto
+	
+	public static void mostraValorUnitQtd(double[] a, int[] b) {
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < b.length; j++) {
+				System.out.println("Valor unitário do objeto " + (i+1) + ":R$" + a[i]);
+				System.out.println();
+			}
+		}
+
+		
+	}
+
 	public static int retornaIndiceMaisVendido(int[] b) {
 		int maiorQtd = b[0];
 		int cont = 0;
-			for(int i = 0; i < b.length; i++) {
-				if(b[i] > maiorQtd) {
-					maiorQtd = b[i];
-					cont++;
-				}
+		for (int i = 0; i < b.length; i++) {
+			if (b[i] > maiorQtd) {
+				 b[i]= maiorQtd;
+				cont++;
 			}
-			return cont;	
+		}
+		return cont;
 	}
-	
-	public static void mostraMaisVendido(int indiceMaisVendido, double[] a){
-		for(int i = 0; i < a.length; i++) {
-			if(i == indiceMaisVendido) {
-				System.out.println("O valor do item mais vendido é: R$"+a[i]);
+
+	public static void mostraIndiceMaisVendido(int indiceMaisVendido) {
+		System.out.println("O índice do objeto mais vendido no vetor é: " + indiceMaisVendido);
+	}
+
+	public static void mostraValorMaisVendido(int indiceMaisVendido, double[] a) {
+		for (int i = 0; i < a.length; i++) {
+			if (i == indiceMaisVendido) {
+				System.out.println("O valor do item mais vendido é: R$" + a[i]);
 			}
 		}
 	}
-	
+
 	public static double calculaValorVendas(double[] a) {
 		int soma = 0;
-		for(int i = 0; i < a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 			soma += a[i];
 		}
 		return soma;
 	}
-	
+
+	public static void mostraValorVendas(double totalVendas) {
+		System.out.println("O valor total das vendas é: R$" + totalVendas);
+	}
+
 	public static double calculaComissao(double totalVendas) {
-		return ((545) + (totalvendas * 0.5));
+		double comissao = 0;
+		comissao = (totalVendas * 0.05);
+		return comissao;
+	}
+	
+	public static void mostraComissao(double comissao) {
+		System.out.println("O valor da comissão a ser pago é de: R$" + comissao);
 	}
 
 }
